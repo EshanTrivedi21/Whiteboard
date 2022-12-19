72b34bc1f52db5a3123b8033528a2acba158d42f
Eshan Trivedi <eshan.trivedi.9@gmail.com>
2022-12-19 23:35:09 +0530

    feat: pencolor eraser clear downloa

--- components/Board.js
+++ components/Board.js
@@ -3,7 +3,10 @@ import { fabric } from "fabric";
 
 export const Board = () => {
   const canvasRef = useRef(null);
-  const [penwidth, setPenwidth] = useState(3);
+  const [penWidth, setPenWidth] = useState(3);
+  const [pencolor, setPencolor] = useState(3);
+  const [eraserWidth, setEraserWidth] = useState(3);
+  const [toggleEraser, setToggleEraser] = useState(false);
   const [fabricCanvas, setFabricCanvas] = useState();
   useEffect(() => {
     const canvas = new fabric.Canvas(canvasRef.current, {
@@ -20,7 +23,46 @@ export const Board = () => {
   const changePenWidth = (width) => {
     if (fabricCanvas) {
       fabricCanvas.freeDrawingBrush.width = width;
-      setPenwidth(width);
+      setPenWidth(width);
+      fabricCanvas.renderAll.bind(fabricCanvas);
+    }
+  };
+  const changePenColor = (color) => {
+    if (fabricCanvas) {
+      fabricCanvas.freeDrawingBrush.color = color;
+      setPencolor(color);
+      fabricCanvas.renderAll.bind(fabricCanvas);
+    }
+  };
+  const downloadBoard = () => {
+    const pngData = fabricCanvas.toDataURL("png");
+    const downloadLink = document.createElement("a");
+    const fileName = "whiteboard.png";
+    downloadLink.href = pngData;
+    downloadLink.download = fileName;
+    downloadLink.click();
+  };
+  const clearBoard = () => {
+    if (fabricCanvas) {
+      fabricCanvas.clear();
+      fabricCanvas.backgroundColor = "white";
+    }
+  };
+  const toggleErase = () => {
+    if (fabricCanvas) {
+      if (toggleEraser) {
+        changePenColor("black");
+        setToggleEraser(false);
+      } else {
+        changePenColor("white");
+        setToggleEraser(true);
+      }
+    }
+  };
+  const changeEraserWidth = (width) => {
+    if (fabricCanvas) {
+      fabricCanvas.freeDrawingBrush.width = width;
+      setEraserWidth(width);
       fabricCanvas.renderAll.bind(fabricCanvas);
     }
   };
@@ -28,14 +70,47 @@ export const Board = () => {
     <div className="container w-full h-full">
       <canvas ref={canvasRef}></canvas>
       <div>
-        <label>Pen Width : {penwidth} </label>
+        <label>Pen Width : {penWidth} </label>
         <input
           type="range"
           onChange={(e) => changePenWidth(e.target.value)}
-          value={penwidth}
+          value={penWidth}
           min={1}
           max={10}
         ></input>
+      </div>
+      <div>
+        <label>Eraser Width : {eraserWidth} </label>
+        <input
+          type="range"
+          onChange={(e) => changeEraserWidth(e.target.value)}
+          value={eraserWidth}
+          min={1}
+          max={25}
+        ></input>
+      </div>
+      <div>
+        <label>Pen Color : {pencolor} </label>
+        <input
+          type="color"
+          onChange={(e) => changePenColor(e.target.value)}
+          value={pencolor}
+        ></input>
+      </div>
+      <div>
+        <button type="button" onClick={() => downloadBoard()}>
+          Download Board
+        </button>
+      </div>
+      <div>
+        <button type="button" onClick={() => clearBoard()}>
+          Clear Board
+        </button>
+      </div>
+      <div>
+        <button type="button" onClick={() => toggleErase()}>
+          {toggleEraser ? "Stop" : "Use"} Erase
+        </button>
       </div>
     </div>
   );
