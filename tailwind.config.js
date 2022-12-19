bd9dbc94b6c9ac8cd85922a01129ad0cd09d9902
Eshan Trivedi <eshan.trivedi.9@gmail.com>
2022-12-19 22:54:46 +0530

    feat: init

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,tsjsx,tsx}",
+    "./components/**/*.{js,tsjsx,tsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
