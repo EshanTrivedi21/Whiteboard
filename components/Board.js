6ae55b17a765779123e8961269092bc1e0ee4405
Eshan Trivedi <eshan.trivedi.9@gmail.com>
2022-12-22 00:52:13 +0530

    styles: major styles update

--- components/Board.js
+++ components/Board.js
@@ -2,22 +2,22 @@ import React, { useEffect, useRef, useState } from "react";
 import { fabric } from "fabric";
 
 export const Board = () => {
-  const canvasRef = useRef(null);
+  const canvasRefPC = useRef(null);
   const [penWidth, setPenWidth] = useState(3);
   const [pencolor, setPencolor] = useState(3);
   const [eraserWidth, setEraserWidth] = useState(3);
   const [toggleEraser, setToggleEraser] = useState(false);
   const [fabricCanvas, setFabricCanvas] = useState();
   useEffect(() => {
-    const canvas = new fabric.Canvas(canvasRef.current, {
-      backgroundColor: "white",
+    const canvas = new fabric.Canvas(canvasRefPC.current, {
+      backgroundColor: "#F9F1F0",
       isDrawingMode: true,
     });
     setFabricCanvas(canvas);
     return () => {
       canvas.dispose();
     };
-  }, [canvasRef]);
+  }, [canvasRefPC]);
   const changePenWidth = (width) => {
     if (fabricCanvas) {
       fabricCanvas.freeDrawingBrush.width = width;
@@ -52,7 +52,7 @@ export const Board = () => {
         changePenColor("black");
         setToggleEraser(false);
       } else {
-        changePenColor("white");
+        changePenColor("#F9F1F0");
         setToggleEraser(true);
       }
     }
@@ -65,55 +65,74 @@ export const Board = () => {
     }
   };
   return (
-    <div className="container w-full h-full flex">
-      <div className="navbarMain">
-      <div>
-        <label>Pen Width : {penWidth} </label>
-        <input
-          type="range"
-          onChange={(e) => changePenWidth(e.target.value)}
-          value={penWidth}
-          min={1}
-          max={10}
-        ></input>
+    <div>
+      <div className="flex mainPC">
+        <div className="navbarMainPC">
+          <h1 className="Heading">The Board.</h1>
+          <div className="componentDiv flex flex-col">
+            <label className="text-xl">Pen Width :</label>
+            <input
+              type="range"
+              onChange={(e) => changePenWidth(e.target.value)}
+              value={penWidth}
+              min={1}
+              max={10}
+            ></input>
+          </div>
+          <div className="componentDiv flex flex-col">
+            <label className="text-xl">Pen Color :</label>
+            <input
+              type="color"
+              onChange={(e) => changePenColor(e.target.value)}
+              value={pencolor}
+              disabled={toggleEraser ? true : false}
+            ></input>
+          </div>
+          <div className="componentDiv flex flex-col">
+            <label className="text-xl">Eraser Width :</label>
+            <input
+              type="range"
+              onChange={(e) => changeEraserWidth(e.target.value)}
+              value={eraserWidth}
+              min={15}
+              max={25}
+              disabled={toggleEraser ? false : true}
+            ></input>
+          </div>
+          <div className="divRandom">
+            <div className="componentDiv flex flex-col">
+              <button
+                type="button"
+                className="button"
+                onClick={() => toggleErase()}
+              >
+                {toggleEraser ? "Use Pen" : "Use Eraser"}
+              </button>
+            </div>
+            <div className="componentDiv flex flex-col">
+              <button
+                type="button"
+                className="button"
+                onClick={() => clearBoard()}
+              >
+                Clear Board
+              </button>
+            </div>
+            <div className="componentDiv flex flex-col">
+              <button
+                type="button"
+                className="button"
+                onClick={() => downloadBoard()}
+              >
+                Download Board
+              </button>
+            </div>
+          </div>
+        </div>
+        <div>
+          <canvas className="canvasMainPC" ref={canvasRefPC}></canvas>
+        </div>
       </div>
-      <div>
-        <label>Eraser Width : {eraserWidth} </label>
-        <input
-          type="range"
-          onChange={(e) => changeEraserWidth(e.target.value)}
-          value={eraserWidth}
-          min={1}
-          max={25}
-        ></input>
-      </div>
-      <div>
-        <label>Pen Color : {pencolor} </label>
-        <input
-          type="color"
-          onChange={(e) => changePenColor(e.target.value)}
-          value={pencolor}
-        ></input>
-      </div>
-      <div>
-        <button type="button" onClick={() => downloadBoard()}>
-          Download Board
-        </button>
-      </div>
-      <div>
-        <button type="button" onClick={() => clearBoard()}>
-          Clear Board
-        </button>
-      </div>
-      <div>
-        <button type="button" onClick={() => toggleErase()}>
-          {toggleEraser ? "Use Pen" : "Use Eraser"}
-        </button>
-      </div>
-      </div>
-      <div> 
-        <canvas className="canvasMain" ref={canvasRef}></canvas>
-      </div>  
     </div>
   );
 };
